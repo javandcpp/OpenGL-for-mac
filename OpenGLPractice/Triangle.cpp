d0f5ba655e1a2841a1542c66862a28fe934c571a
loneswordman <javandoc1985@gmail.com>
2018-10-17 14:06:40 +0800

    trangle

---
+++ OpenGLPractice/Triangle.cpp
@@ -0,0 +1,195 @@
+//
+//  Triangle.cpp
+//  OpenGLPractice
+//
+//  Created by developer on 2018/10/16.
+//  Copyright © 2018 developer. All rights reserved.
+//
+
+
+#include "Triangle.h"
+Triangle::Triangle(){
+    
+}
+Triangle::~Triangle(){
+    
+}
+void Triangle::draw(){
+    using namespace std;
+
+    glfwInit();
+    //    主版本
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    //    次版本
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    //    告诉glfw 我们使用的是core_profile 核心模块
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //    向前兼容
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    //    创建一个GLFW 窗口   宽 高  窗口名字  后边两个暂时不用管
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return;
+    }
+    //    通知GLFW将我们窗口的上下文设置为当前线程的主上下文了
+    glfwMakeContextCurrent(window);
+    
+    //    GLAD 是用来管理openGL 的函数指针的。所以在调用任何openGL函数之前我们都需要初始化GLAD。
+    //    我们给GLAD传入了用来加载系统相关的OpenGL函数指针地址的函数。GLFW给我们的是glfwGetProcAddress，它根据我们编译的系统定义了正确的函数。
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return;
+    }
+    
+    GLuint vertexShader=glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader,1,&vertexShaderSource,NULL);
+    glCompileShader(vertexShader);
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader,GL_COMPILE_STATUS,&success);
+    cout<<"vertex shader compile "<<success<<endl;
+    if(!success){
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        return;
+    }
+    
+    GLuint fragmentShader=glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader,1,&fragShaderSource,NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader,GL_COMPILE_STATUS,&success);
+    if(!success){
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        return;
+    }
+    
+    GLuint program=glCreateProgram();
+    glAttachShader(program,vertexShader);
+    glAttachShader(program,fragmentShader);
+    glLinkProgram(program);
+    glGetProgramiv(program,GL_LINK_STATUS,&success);
+    if (!success) {
+        glGetProgramInfoLog(program, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    float vertices[]={
+        -0.5f, -0.5f, -0.5f,
+        0.5f, -0.5f, -0.5f,
+        0.5f, 0.5f, -0.5f,
+        0.5f, 0.5f, -0.5f,
+        -0.5f, 0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        
+        -0.5f, -0.5f, 0.5f,
+        0.5f, -0.5f, 0.5f,
+        0.5f, 0.5f, 0.5f,
+        0.5f, 0.5f, 0.5f,
+        -0.5f, 0.5f, 0.5f,
+        -0.5f, -0.5f, 0.5f,
+        
+        -0.5f, 0.5f, 0.5f,
+        -0.5f, 0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f, 0.5f,
+        -0.5f, 0.5f, 0.5f,
+        
+        0.5f, 0.5f, 0.5f,
+        0.5f, 0.5f, -0.5f,
+        0.5f, -0.5f, -0.5f,
+        0.5f, -0.5f, -0.5f,
+        0.5f, -0.5f, 0.5f,
+        0.5f, 0.5f, 0.5f,
+        
+        -0.5f, -0.5f, -0.5f,
+        0.5f, -0.5f, -0.5f,
+        0.5f, -0.5f, 0.5f,
+        0.5f, -0.5f, 0.5f,
+        -0.5f, -0.5f, 0.5f,
+        -0.5f, -0.5f, -0.5f,
+        
+        -0.5f, 0.5f, -0.5f,
+        0.5f, 0.5f, -0.5f,
+        0.5f, 0.5f, 0.5f,
+        0.5f, 0.5f, 0.5f,
+        -0.5f, 0.5f, 0.5f,
+        -0.5f, 0.5f, -0.5f,
+
+    
+    };
+    
+    unsigned int VBO,VAO;
+    glGenVertexArrays(1,&VAO);
+    glGenBuffers(1,&VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void *)NULL);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    glBindVertexArray(0);
+    
+    
+    
+    
+    
+    
+    
+    
+    //    创建渲染的视口: 我们必须要告诉OpenGl 需要渲染的尺寸大小，即为视口 viewport(),这样openGL 才能知道根据窗口大小显示数据和坐标。
+    //    glViewport 前两个参数控制视口左下角位置，后两个参数控制视口的宽和高
+    //    openGL 幕后使用的是glViewport 定义的 位置和宽高进行2D转换
+    glViewport(0, 0, 800, 600);
+    //    窗口调整的时候 视口应该也被调整  对窗口注册一个回调函数每次窗口大小被调整的时候会被调用
+    void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+    
+    
+    //   通过 glfwSetFramebufferSizeCallback glfw 函数 当窗口frame 变化时会调用。
+    //    对于视网膜屏 Retain 屏   宽度和高度明显比原输入值更高一点。
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    void processInput(GLFWwindow *window);
+    
+    //   为了防止 渲染的图像一出现就退出 我们使用while 循环 。我们可以称之为Render Loop
+    //    glfwWindowShouldClose 每次循环开始前检查一次GLFW 是否被要求退出 是true 的话渲染便结束了。
+    while(!glfwWindowShouldClose(window))
+    {
+        //输出控制
+        processInput(window);
+        //        glfwSwapBuffers 会交换颜色缓冲（他是存储着GLFW 窗口每一个像素色值的大缓冲），将会作为输出显示在屏幕上
+        //        当程序退出的时候 使用一个自定义的颜色清空屏幕  在每个新的渲染迭代可是的时候我们总希望清屏否则总是看到上次渲染的结果。
+        //        我们可以使用glClear   GL_COLOR_BUFFER_BIT，GL_DEPTH_BUFFER_BIT和GL_STENCIL_BUFFER_BIT。 我们清空颜色 。
+        //        glClearColor(0.5f, 0.1f, 0.0f, 1.0f);
+        glClearColor(0.0f,0.0f,0.0f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glUseProgram(program);
+        
+        glm::mat4 transform;
+        transform = glm::rotate(transform, 10*(float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f)); //绕y轴旋转
+        
+        unsigned int transformLoc = glGetUniformLocation(program, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+        
+     
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES,0,36);
+        glfwSwapBuffers(window);
+        //        glfwPollEvents 检查函数有没有触发什么事件 键盘输入 鼠标移动 并调用对应函数
+        glfwPollEvents();
+    }
+    
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glfwTerminate();
+    
+}
---
+++ OpenGLPractice/Triangle.h
@@ -0,0 +1,45 @@
+//
+//  Triangle.hpp
+//  OpenGLPractice
+//
+//  Created by developer on 2018/10/16.
+//  Copyright © 2018 developer. All rights reserved.
+//
+
+#ifndef Triangle_hpp
+#define Triangle_hpp
+#include <iostream>
+#include <stdio.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+#define GETSHADER(x) #x
+
+static const char *vertexShaderSource="#version 330 core \n"
+"layout (location = 0) in vec3 aPos;\n"
+"uniform mat4 transform;\n"
+"void main(){\n"
+"gl_Position=transform * vec4(aPos,1.0);\n"
+"}\0";
+
+
+static const char *fragShaderSource="#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+
+class Triangle{
+public:
+    Triangle();
+    virtual ~Triangle();
+    void draw();
+    
+};
+
+#endif /* Triangle_hpp */
